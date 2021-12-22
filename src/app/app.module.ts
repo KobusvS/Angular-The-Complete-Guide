61f6841d4f394bfacaffda875578eb10fbbcabfd
Kobus <kobus@infinityarc.net>
2021-12-22 12:20:47 +0200

    reworked app components, removed old components and added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { ShoppingListEditComponent } from './Components/shopping-list/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { ShoppingListComponent } from './Components/shopping-list/shopping-list/shopping-list.component';
-import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './Components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,15 +11,19 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent
+    RecipeDetailsComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent
+
+
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipes.component.spec.ts
+++ src/app/recipes/recipes.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RecipeItemComponent } from './recipe-item.component';
+import { RecipesComponent } from './recipes.component';
 
-describe('RecipeItemComponent', () => {
-  let component: RecipeItemComponent;
-  let fixture: ComponentFixture<RecipeItemComponent>;
+describe('RecipesComponent', () => {
+  let component: RecipesComponent;
+  let fixture: ComponentFixture<RecipesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RecipeItemComponent ]
+      declarations: [ RecipesComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RecipeItemComponent);
+    fixture = TestBed.createComponent(RecipesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
