2246ad8e5afe633253ff854dee680f28e7be198e
Kobus <kobus@infinityarc.net>
2021-12-22 10:00:33 +0200

    updated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListEditComponent } from './Components/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './Components/recipes/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListEditComponent } from './Components/shopping-list/shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { ShoppingListComponent } from './Components/shopping-list/shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Components/recipes/recipe-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ShoppingListComponent } from './Components/shopping-list/shopping-list/
     ShoppingListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
